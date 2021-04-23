4ca69c66b8bd287b3a427e0536dabd20bcac1152
Steve Dennis <s.r.dennis@liverpool.ac.uk>
2021-04-23 05:16:28 -0500

    add nuevent content back

--- core/NuEvent.py
+++ core/NuEvent.py
@@ -7,5 +7,30 @@ float_default = -999.
 class NuEvent:
   run:    int = int_default
   subrun: int = int_default
+  event:  int = int_default
+
+  selection:int = int_default
+    
+  nu_pdg_init:int = int_default#same with nu_pdg_final
+  nu_pdg_final:int = int_default
+  #nu_interaction_ccnc:int = int_default#IsNC?
+  IsNC: int = int_default
+  nu_interaction_mode:int = int_default
+
+  enu_true:float = float_default # both in MeV
+  enu_reco:float = float_default # both in MeV
+  #nu_energy_true:float = float_default
+  #nu_energy_reco:float = float_default
+  event_weight:float = float_default#(xsec_corr_weight)
+  lepton_theta_reco:float = float_default#reco lepton angle
+  lepton_momentum_reco:float = float_default#Not yet needed
   
-  enu_true: float = float_default
+  reco_nuvtx_x:float = float_default
+  reco_nuvtx_y:float = float_default
+  reco_nuvtx_z:float = float_default
+  
+  numu_cc_flag:int = int_default
+  nue_cc_flag:int = int_default
+  
+  numu_score:float = float_default
+  nue_score:float = float_default
