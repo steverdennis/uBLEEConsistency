290848785bfa348778b50a0267f2a3e037df9f99
Steve Dennis <s.r.dennis@liverpool.ac.uk>
2021-05-14 14:29:58 -0500

    add enum values to Identifiers.py

---
+++ core/Identifiers.py
@@ -0,0 +1,32 @@
+
+# MC production file types
+kMC_BNB  = 0
+kMC_nue  = 1
+kMC_dirt = 2
+kMC_ext  = 2
+
+# Interaction modes
+kElastic   = 0
+kResonance = 1
+kMEC       = 10
+
+# PDG codes
+kNuE      =  12
+kNuMu     =  14
+kNuTau    =  16
+kNuEBar   = -12
+kNuMuBar  = -14
+kNuTauBar = -16
+
+kElectron     =  11
+kMuon         =  13
+kTau          =  15
+kAntiElectron = -11
+kAntiMuon     = -13
+kAntiTau      = -15
+
+kProton  = 2212
+kNeutron = 2112
+kPiPlus  =  211
+kPiMinus = -211
+kPiZero  =  111
