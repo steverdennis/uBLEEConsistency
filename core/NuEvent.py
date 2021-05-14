f4dfd6716175fc7aca70b489951cb39baccff67b
Steve Dennis <s.r.dennis@liverpool.ac.uk>
2021-05-14 14:30:50 -0500

    document units for angles in NuEvent

--- core/NuEvent.py
+++ core/NuEvent.py
@@ -11,29 +11,29 @@ class NuEvent:
 
   selection:int = int_default
     
-  nu_pdg_init:int = int_default#same with nu_pdg_final
-  nu_pdg_final:int = int_default
-  #nu_interaction_ccnc:int = int_default#IsNC?
-  IsNC: int = int_default
+  nu_pdg_init:int         = int_default #same with nu_pdg_final
+  nu_pdg_final:int        = int_default
+  IsNC: int               = int_default
   nu_interaction_mode:int = int_default
-
-  enu_true:float = float_default # both in MeV
-  enu_reco:float = float_default # both in MeV
   
-  event_weight:float = float_default     
-  lepton_theta_reco:float = float_default
-  lepton_energy_reco:float = float_default
-  proton_theta_reco:float = float_default
-  proton_energy_reco:float = float_default
+  event_weight:float       = float_default # product of all included weights
+    
+  enu_true:float = float_default # MeV
+  enu_reco:float = float_default # MeV
+  
+  lepton_theta_reco :float  = float_default # degrees
+  lepton_energy_reco:float  = float_default # MeeV
+  proton_theta_reco :float  = float_default # degrees
+  proton_energy_reco:float  = float_default # MeV
   
   reco_nuvtx_x:float = float_default
   reco_nuvtx_y:float = float_default
   reco_nuvtx_z:float = float_default
   
-  # WC
+  # Wire-Cell BDTs
   numu_cc_flag:int = int_default
-  nue_cc_flag:int = int_default
-  match_isFC:int = int_default
+  nue_cc_flag:int  = int_default
+  match_isFC:int   = int_default
   
   numu_score:float = float_default
-  nue_score:float = float_default
+  nue_score:float  = float_default
